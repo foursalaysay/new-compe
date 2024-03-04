bfd67bf723cc67bbab7268f7e25f4fec10ac2c78
John Kyle Salaysay <foursalaysay@gmail.com>
2024-03-04 13:11:37 +0800

    redefining code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,36 @@ import Logo from './../../public/logo.png'
 
 export default function Home() {
   return (
-    <>
+    <div className='w-screen h-auto p-0'>
+      <div className=' w-screen h-screen flex flex-row justify-center gap-20 items-center'>
     <Image
     src={Logo}
     alt='image-landing'
     width={500}
-    height={500}
+    height={500}                
+
     />
-    </>
-  );
-}
+    <div className='flex flex-col text-left gap-5'>
+      <h1 className='text-8xl font-bold'>
+        ShareSurplus
+      </h1>
+      <p className='text-lg'>Companies donated products delivered at ease</p>
+    </div>
+    
+    </div>
+
+    {/* ABOUT SHARESURPLUS */}
+
+    <div className='h-screen w-screen flex flex-row'>
+      <section className='flex flex-col gap-10'>
+        <h1 className='text-4xl'>About</h1>
+        <div className='bg-gray-400 rounded h-2/3 w-[500px]'>
+
+        </div>
+      </section>
+      
+    </div>
+    </div>
+    
+  )
+};
